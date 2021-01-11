176680fdb3448a90f8b21a6ebbc7232e1e5a3d4f
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2021-01-11 14:23:59 -0500

    fixed length

--- Radix.java
+++ Radix.java
@@ -1,8 +1,17 @@
-public class Radix extends class MyLinkedList{
+public class Radix{
 
   public static int nth(int a, int col) {
     String n = String.valueOf(a);
     return Integer.parseInt(String.valueOf(n.charAt(n.length() - 1 - col)));
   }
 
+  public static int length(int a) {
+    String n = String.valueOf(a);
+    if (n.charAt(0) == '-') return n.length() - 1;
+    else return n.length();
+  }
+
+  // public static void merge(MyLinkedList original,MyLinkedList[]buckets) {
+  // 
+  // }
 }
