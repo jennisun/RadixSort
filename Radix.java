6c434d4a6649ecfd9214a88e28034f0c36cce68d
jennisun <jennifersun@Jennifers-MacBook-Air-2.local>
2021-01-14 21:13:48 -0500

    zoom zoom zoom

--- Radix.java
+++ Radix.java
@@ -25,12 +25,12 @@ public class Radix{
       for (int j = 0; j < 10; j ++) buckets[j] = new SortableLinkedList();
 
       while (data.size() != 0) {
-        maxlen = Math.max(maxlen, length(data.get(0)));
-        if (k < length((int)data.get(0))) buckets[nth(data.get(0), k)].add(data.get(0));
-        else buckets[0].add(data.get(0));
+        int value = data.get(0);
+        maxlen = Math.max(maxlen, length(value));
+        if (k < length(value)) buckets[nth(value, k)].add(value);
+        else buckets[0].add(value);
         data.remove(0);
       }
-
       merge(data, buckets);
     }
   }
@@ -43,9 +43,7 @@ public class Radix{
     while (data.size() != 0) {
       int value = (int)data.get(0);
       if (value >= 0) pos.add(value);
-      else {
-        neg.add(value * -1);
-      }
+      else neg.add(value * -1);
       data.remove(0);
     }
 
